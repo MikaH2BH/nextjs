f59e562694446b3d2bfb36f30728a1c26a459afd
Mika <47526162+TheMikaMan@users.noreply.github.com>
2023-07-28 13:06:11 +0200

    ee

--- app/page.js
+++ app/page.js
@@ -7,11 +7,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-          <h1>Boos op Dierenkliniek Maastricht</h1>
-          <p>
-              Hier komt mijn verhaal. Ik ben Mango en ik ben heel boos op Dierenkliniek Maastricht. 
-              Mijn baasje had bijna mijn pootje willen amputeren of mij zelfs laten inslapen.
-          </p>
+        <div className={row}>
+            <h1>Boos op Dierenkliniek Maastricht</h1>
+            <h5>
+                Hier komt mijn verhaal. Ik ben Mango en ik ben heel boos op Dierenkliniek Maastricht. 
+                Mijn baasje had bijna mijn pootje willen amputeren of mij zelfs laten inslapen.
+            </h5>
+          </div>
         <div>
           
         </div>
