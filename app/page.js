2bda27bc8fda7fd6d6ed940ded8b5ef4e41e51ed
Mika <47526162+TheMikaMan@users.noreply.github.com>
2023-07-28 14:10:26 +0200

    eeeeeeeeeeeeeeee

--- app/page.js
+++ app/page.js
@@ -10,6 +10,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Verhaal van Mango <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Andere klachten</a>
+            </li>
+            
+          </ul>
+        </div>
+      </nav>
       <div className={styles.grid}>
         <div>
           <h1>Boos op Dierenkliniek Maastricht</h1>
